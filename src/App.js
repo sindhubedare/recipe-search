90e8ab2226220dcb95c0859f30faaeecdf6898c1
sindhubedare <sindhubedare1990@gmail.com>
2020-08-14 16:23:16 -0500

    Search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Components/Header'
+import Search from './Search/Search';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Search />
     </div>
   );
 }
---
+++ src/Search/Search.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => {
+    return(
+      
+  <div className="row">
+  <form className="col s12">
+    <div className="row">
+      <div className="input-field col s6">
+        <input placeholder="Ingredient" id="first_name" type="text" className="validate" />
+        {/* <label htmlFor="first_name">First Name</label> */}
+       </div>
+      </div>
+    </form>
+  </div>
+    )
+}

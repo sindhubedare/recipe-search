020d62b10dd6412397ce51b1a192d380295cec4d
sindhubedare <sindhubedare1990@gmail.com>
2020-08-14 15:36:31 -0500

    psuedocode

---
+++ src/Psuedocode.js
@@ -0,0 +1,8 @@
+//Create wireframe
+//Create components and files
+//create header
+//create search bar and submit button
+// fetch api
+//Pass api through serach 
+//generate results with images
+//do css and styling

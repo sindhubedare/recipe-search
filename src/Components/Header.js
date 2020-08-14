3ca24fb9286537cb24f2d4b2911ddda51c6994b5
sindhubedare <sindhubedare1990@gmail.com>
2020-08-14 15:59:57 -0500

    components

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header () {
+    return(
+        <div>
+            <h1 className= "header-1"> RECIPE SEARCH FOR PUPPY </h1>
+        </div>
+    )
+}
+ export default Header;

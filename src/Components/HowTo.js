bf2026a320f605cbd8594ddc5e78ab20f531d2b5
sindhubedare <sindhubedare1990@gmail.com>
2020-08-18 13:48:53 -0500

    div added

--- src/Components/HowTo.js
+++ src/Components/HowTo.js
@@ -3,5 +3,12 @@ import React from 'react';
 //Stateless function to display the information of the website
 
 export default function HowTo(){
-    
+    return(
+        <React.Fragment>
+            <div className= "how-to">
+                
+            </div>
+        </React.Fragment>
+    )
+
 }

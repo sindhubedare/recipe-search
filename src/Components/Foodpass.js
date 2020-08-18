b7623af5b4ee1197be607e588635c980547ac992
sindhubedare <sindhubedare1990@gmail.com>
2020-08-18 09:46:55 -0500

    foodpasser ready

--- src/Components/Foodpass.js
+++ src/Components/Foodpass.js
@@ -1,3 +1,9 @@
 import React from 'react';
-
+function Foodpasser (){
+    return (
+        <div>
+            <img src= "https://cdn.dribbble.com/users/436306/screenshots/6026974/foodline.gif" alt= "food-passer"/>
+        </div>
+    )
+}
 export default Foodpasser;

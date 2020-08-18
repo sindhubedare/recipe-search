df500a67f256a3ecbf3101a3a87aa4f22f8b92ad
sindhubedare <sindhubedare1990@gmail.com>
2020-08-18 14:20:01 -0500

    changes

--- src/Content/Result.js
+++ src/Content/Result.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom'
-import HowTo from '../Components/HowTo';
+
 // export default () => {
 //     return (
 //         <div>
@@ -9,34 +8,3 @@ import HowTo from '../Components/HowTo';
 //     )
 // }
 
-export default function Result() {
-    return (
-  
-    <Router>
-       <nav>
-           <ul className="lists-display">
-           <li>  <Link to = '/'>Home </Link> </li>
-               <li>
-                  <Link to ='/Search'> Search</Link>
-               </li>
-               {/* <li>
-                   <Link to = '/GenerateMeme'>Create Captions for Meme</Link>
-               </li> */}
-               
-               <li><Link to = '/HowTo'> Instructions to generate Meme</Link></li>
-           </ul>   
-        </nav>
-          <Switch>
-   
-               <Route exact path="/Search"  component={Search}/>
-               {/* <Route path="/GenerateMeme" component={GenerateMeme}/> */}
-               <Route path="/HowTo" component={HowTo} />
-               <Route path="/" component={About} />  
-          </Switch>
-     
-    </Router>
-
-
-   
-    )
-}

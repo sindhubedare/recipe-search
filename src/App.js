f5c14f35ffbb9272c28a3d77525cb681fcbcf10a
sindhubedare <sindhubedare1990@gmail.com>
2020-08-18 11:57:44 -0500

    format

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import './App.css';
-import Header from './Components/Header'
-import Search from './Search/Search'
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Search from "./Search/Search";
 
 function App() {
   return (
     <div className="App">
-     <Header />
-     <Search />
+      <Header />
+      <Search />
     </div>
   );
 }

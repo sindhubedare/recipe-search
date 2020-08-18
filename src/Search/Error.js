253ef9afa61ff77246fcb6d6f99fa54a66a1e2ea
sindhubedare <sindhubedare1990@gmail.com>
2020-08-18 10:47:47 -0500

    error component

--- src/Search/Error.js
+++ src/Search/Error.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-export default function erro(props) {
-    return(
-        <div>
-            <h1 className ="error-msg">{props.message}</h1>
-        </div>
-    )
+class Error extends React.Component{
+
 }
+
+export default Error;

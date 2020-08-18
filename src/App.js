952333ef8f17d3239d4107f15547e1e8d8651bfb
sindhubedare <sindhubedare1990@gmail.com>
2020-08-18 13:55:17 -0500

    app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import Search from "./Search/Search";
+import Result from "./Components/HowTo";
 
 function App() {
   return (
     <div className="App">
+      <Result />
       <Header />
       <Search />
     </div>

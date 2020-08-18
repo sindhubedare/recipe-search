4f892faf565692d393e0185c6fa3936ae96173fa
sindhubedare <sindhubedare1990@gmail.com>
2020-08-18 13:42:43 -0500

    router dom

--- src/Content/Result.js
+++ src/Content/Result.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
-export default () => {
+// export default () => {
+//     return (
+//         <div>
+//             <h1> Result</h1>
+//         </div>
+//     )
+// }
+
+export default function Result() {
     return (
-        <div>
-            <h1> Result</h1>
-        </div>
+  
+    <Router>
+       <nav>
+           <ul className="lists-display">
+           <li>  <Link to = '/'>Home </Link> </li>
+               <li>
+                  <Link to ='/Search'> Search</Link>
+               </li>
+               {/* <li>
+                   <Link to = '/GenerateMeme'>Create Captions for Meme</Link>
+               </li> */}
+               
+               <li><Link to = '/HowTo'> Instructions to generate Meme</Link></li>
+           </ul>   
+        </nav>
+          <Switch>
+   
+               <Route exact path="/Search"  component={Search}/>
+               <Route path="/GenerateMeme" component={GenerateMeme}/>
+               <Route path="/HowTo" component={HowTo} />
+               <Route path="/" component={About} />  
+          </Switch>
+     
+    </Router>
+
+
+   
     )
 }

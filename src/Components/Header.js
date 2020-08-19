375c2b10669298885e34f65c98fb23e17af3d005
sindhubedare <sindhubedare1990@gmail.com>
2020-08-18 21:29:05 -0500

    header-class

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,30 @@
-import React from 'react';
+// import React from 'react';
+// // import Foodpass from './Foodpass';
+
+// class Header extends React.Component{
+//     state={}
+//     render(){
+//     return(
+//         <div>
+//         <div className="card-panel white-text teal lighten-2">
+//             <h4>RECIPE SEARCH</h4>
+//         </div>
+//         {/* <div className="food-pass">
+//         <Foodpass />
+//         </div> */}
+        
+//         </div>
+//     )
+// }
+// }
+//  export default Header;
+
+ import React from 'react';
 // import Foodpass from './Foodpass';
 
-function Header () {
+class Header extends React.Component{
+    state={}
+    render(){
     return(
         <div>
         <div className="card-panel white-text teal lighten-2">
@@ -14,7 +37,9 @@ function Header () {
         </div>
     )
 }
+}
  export default Header;
+
 
 
 

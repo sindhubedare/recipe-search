1afbcf886abf9f9d057a563e166fc65854f9f1fa
sindhubedare <sindhubedare1990@gmail.com>
2020-08-18 20:26:03 -0500

    header issue- need fix

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,20 @@
-// import React from 'react';
-// import Foodpass from './Foodpass';
+import React from 'react';
+import Foodpass from './Foodpass';
 
-// function Header () {
-//     return(
-//         <div>
-//         {/* <div className="card-panel white-text teal lighten-2">
-//             <h4>RECIPE SEARCH</h4>
-//         </div> */}
-//         <div className="food-pass">
-//         <Foodpass />
-//         </div>
+function Header () {
+    return(
+        <div>
+        <div className="card-panel white-text teal lighten-2">
+            <h4>RECIPE SEARCH</h4>
+        </div>
+        {/* <div className="food-pass">
+        <Foodpass />
+        </div> */}
         
-//         </div>
-//     )
-// }
-//  export default Header;
+        </div>
+    )
+}
+ export default Header;
 
 
 
--- src/Content/Recipe.js
+++ src/Content/Recipe.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import Header from '../Components/Header';
+import Header from '../Components/Header';
 import Search from '../Search/Search'
 
 export default () => {

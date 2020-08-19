730ec75cf4a1ec9e69d35b7e6d68119a5816c0fc
sindhubedare <sindhubedare1990@gmail.com>
2020-08-18 21:35:51 -0500

    class component ready

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -23,20 +23,17 @@
 // import Foodpass from './Foodpass';
 
 class Header extends React.Component{
-    state={}
-    render(){
-    return(
-        <div>
-        <div className="card-panel white-text teal lighten-2">
-            <h4>RECIPE SEARCH</h4>
-        </div>
-        {/* <div className="food-pass">
-        <Foodpass />
-        </div> */}
-        
-        </div>
-    )
-}
+ constructor(props) {
+     super(props);
+     this.header = "Find me"
+ }
+ render (){
+     return(
+     <div>
+         {this.header};
+     </div>
+     );
+ }
 }
  export default Header;
 

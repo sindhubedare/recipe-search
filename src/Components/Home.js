e7a4a229064bd0540e713ac5dd51e5408a2b5dd7
sindhubedare <sindhubedare1990@gmail.com>
2020-08-18 22:33:25 -0500

    final changes

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 import "../App.css";
-function Home (){
-    return (
-        <div className= "home-gif">
-            <h5> Welcome to Sindhu's recipe search</h5>
-            {/* <img src= "https://cdn.dribbble.com/users/208061/screenshots/2663861/movin-food.gif" alt= "Home" width="200pt" height="200pt"/> */}
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <div>
+        <h5 className="text"> Welcome to Sindhu's recipe search</h5>
+      </div>
+
+      {/* <img src= "https://cdn.dribbble.com/users/208061/screenshots/2663861/movin-food.gif" alt= "Home" width="200pt" height="200pt"/> */}
+
+      <div className="home-gif"> </div>
+    </div>
+  );
 }
 export default Home;
 
